445428ae28251cf5c3ec3c5f1fd0862400c228e4
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-27 00:07:35 +0100

    homegage

--- public/index copy.php
+++ public/index copy.php
@@ -6,10 +6,20 @@
 
 <head>
     <?php $BRC_TEMPLATE_PATH = parse_url(get_template_directory_uri(), PHP_URL_PATH); ?>
+    <script
+        src='<?php echo $BRC_TEMPLATE_PATH; ?>/react-src/node_modules/@devloco/create-react-wptheme-utils/wpThemeClient.js'>
+    </script>
 
+    <script
+        src='<?php echo $BRC_TEMPLATE_PATH; ?>/react-src/node_modules/@devloco/create-react-wptheme-utils/wpThemeErrorOverlay.js'>
+    </script>
+
+    <script>
+    wpThemeClient.start("ws", "127.0.0.1", "8090");
+    </script>
 
     <meta charset="utf-8" />
-    <link rel="shortcut icon" href="/wp-content/themes/tienda_projecto/favicon.ico" />
+    <link rel="shortcut icon" href="/wp-content/themes/tienda/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <meta name="description" content="Web site created using create-react-wptheme" />
@@ -23,17 +33,17 @@
         If you're reading this from "view source" in your browser, it might not make sense as
         these tokens have already been evaluated and replaced, even in this remark blurb.
 
-        Notice the use of "php echo $TEMPLATE_PATH;" and /wp-content/themes/tienda_projecto in the tags above.
-        Both will be replaced with the URL of the `public` folder during the build (/wp-content/themes/tienda_projecto) or
+        Notice the use of "php echo $TEMPLATE_PATH;" and /wp-content/themes/tienda in the tags above.
+        Both will be replaced with the URL of the `public` folder during the build (/wp-content/themes/tienda) or
         at render time (php echo $TEMPLATE_PATH;)
         Only files inside the `public` folder can be referenced like this.
 
-        Unlike "/favicon.ico" or "favicon.ico", "/wp-content/themes/tienda_projecto/favicon.ico" will
+        Unlike "/favicon.ico" or "favicon.ico", "/wp-content/themes/tienda/favicon.ico" will
         work correctly both with client-side routing and a non-root public URL.
         Learn how to configure a non-root public URL by running `npm run wpbuild`.
     -->
     <title>React WordPress Theme</title>
-    <link href="/wp-content/themes/tienda_projecto/static/css/main.854d246b.css" rel="stylesheet">
+    <link href="/wp-content/themes/tienda/static/css/main.854d246b.css" rel="stylesheet">
 </head>
 
 <body>
@@ -51,7 +61,7 @@
         To begin the development, run `npm run wpstart` or `yarn wpstart`.
         To create a production bundle, use `npm run wpbuild` or `yarn wpbuild`.
     -->
-    <script src="/wp-content/themes/tienda_projecto/static/js/main.455bdc6c.js"></script>
+    <script src="/wp-content/themes/tienda/static/js/main.6f80910f.js"></script>
 
 
 </body>
