69eaeb74c57bb56b965e1fd8f3dcd198e633c8f8
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-27 12:06:57 +0100

    navigate to /

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./index.scss";
 
 import "bootstrap/dist/js/bootstrap.bundle";
@@ -8,7 +8,6 @@ import Layout from "./pages/Layout/Layout";
 import Home from "./pages/Home/Home";
 import Blogs from "./pages/Blogs/Blogs";
 import Contact from "./pages/Contact/Contact";
-import NoPage from "./pages/NoPage/NoPage";
 
 export default function App() {
   return (
@@ -18,7 +17,7 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </BrowserRouter>
